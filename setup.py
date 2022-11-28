ec542eddf6cebed61edcb68741efc471f749eedd
Florian Matter <florianmatter@gmail.com>
2022-11-27 21:57:52 -0800

    license and citation

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="indicogram",
-    version="0.0.1",
+    version="0.0.1.dev",
     description="indicogram",
     classifiers=[
         "Programming Language :: Python",
