8849a9fd0f337c68ec7c38eb901b327bf50c4ab4
Florian Matter <florianmatter@gmail.com>
2023-06-27 17:14:54 -0700

    >=

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         "clld_corpus_plugin>=0.0.8",
         "clld_document_plugin>=0.0.5",
         "clld_morphology_plugin>=0.0.10",
-        "clld-markdown-plugin=>0.2.0",
+        "clld_markdown_plugin>=0.2.0",
         "waitress>=2.1.2",
         "cldf-ldd"
     ],
